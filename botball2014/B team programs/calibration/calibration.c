570b2e9733b88582d64353e0bf1689720f901fdc
KSMSbotball <ksmsbotball123@gmail.com>
2014-02-06 16:48:31 -0500

    finished calibration program

--- botball2014/B team programs/calibration/calibration.c
+++ botball2014/B team programs/calibration/calibration.c
@@ -1,7 +1,23 @@
-// Created on Thu February 6 2014
-
+#define LEFT_MOTOR_FORWARD 50
+#define RIGHT_MOTOR_FORWARD 50
+#define PORT_LEFT 0
+#define PORT_RIGHT 2
+void forward(int time);
 int main()
 {
-	printf("Hello, World!\n");
+	forward(3);
+	while (a_button() == 0){
+		msleep(50);
+	}
+	forward(10);
+	while (a_button() == 0){
+		msleep(50);
+	}
+	forward(15);
 	return 0;
+}
+void forward(int time){
+	motor(PORT_LEFT,LEFT_MOTOR_FORWARD);
+	motor(PORT_RIGHT,RIGHT_MOTOR_FORWARD);
+	msleep(time * 1000);
 }
