2512e35e737f4ca26b4ca9b5aeb92eb859ccd306
KSMSbotball <ksmsbotball123@gmail.com>
2014-02-14 15:44:06 -0500

    first points!!!!!!

--- botball2014/B team programs/program/program.c
+++ botball2014/B team programs/program/program.c
@@ -1,13 +1,15 @@
 void claw_up();
-void forward (int seconds);
+void forward (double seconds);
 void claw_down ();
-void left_turn();
+void left_turn(double seconds);
 #define LEFT_MOTOR 0
+#define RIGHT_MOTOR 2
 int main(){
 	enable_servos();
 	claw_up();
-	forward(3.8);
-	left_turn();
+	forward(4);
+	left_turn(1.3);
+	forward(1.1);
 	disable_servos();
 	return 0;
 }
@@ -17,13 +19,14 @@ void claw_up () {
 void claw_down () {
 	set_servo_position(0,315);
 }
-void forward (int seconds){
+void forward (double seconds){
 motor(LEFT_MOTOR,500);
-	motor(2,500);
+	motor(RIGHT_MOTOR,500);
 	msleep(seconds * 1000);
 	ao();
 }
-void left_turn(){
+void left_turn(double seconds){
 motor(LEFT_MOTOR,500);
-msleep(765);
+msleep(seconds * 1000);
+	ao();
 }
