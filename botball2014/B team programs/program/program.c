e9dc7197ed274b6358447d33e300648228c58dbc
KSMSbotball <ksmsbotball123@gmail.com>
2014-02-14 11:30:37 -0500

    finished first program

--- botball2014/B team programs/program/program.c
+++ botball2014/B team programs/program/program.c
@@ -1,19 +1,14 @@
 void claw_up();
 void forward (int seconds);
 void claw_down ();
+void left_turn();
+#define LEFT_MOTOR 0
 int main(){
 	enable_servos();
-	claw_up ();
+	claw_up();
 	forward(3.8);
-	
-	
-	
-	motor(0,-500);
-	motor(2,-420);
-	msleep(765);
-	
+	left_turn();
 	disable_servos();
-	
 	return 0;
 }
 void claw_up () {
@@ -23,12 +18,12 @@ void claw_down () {
 	set_servo_position(0,315);
 }
 void forward (int seconds){
-motor(0,500);
+motor(LEFT_MOTOR,500);
 	motor(2,500);
 	msleep(seconds * 1000);
 	ao();
 }
 void left_turn(){
-motor(0,500);
+motor(LEFT_MOTOR,500);
 msleep(765);
 }
