592eb51eee548c9f4ee97ba54d387dca41ec42da
KSMSbotball <botball2014>
2014-02-14 22:13:02 -0500

    program for both teams
    mock for first points

---
+++ botball2014/A Team programs/Milestone1/Milestone1.c
@@ -0,0 +1,72 @@
+// Created on Thu February 6 2014
+//quick and dirty program to see if mav API is more reliable than motor
+//to improve dead recknoning outcome.
+#define RIGHT_MOTOR 2
+#define LEFT_MOTOR 0
+#define SPEED_FWD 700
+#define SPEED_BWD -700
+#define RIGHT 76
+#define LEFT 57
+#define RIGHT_ANGLE_CLICKS 1400
+//declaration
+void moveForward(int distanceInInches); 
+void moveBackward(int distanceInInces);
+void rightAngle(int direction);
+
+int main()
+{
+<<<<<<< HEAD
+	printf("test calibration 1.4\n");
+	
+	printf("testing right angle turn");
+	moveForward(30);
+	rightAngle(RIGHT);
+	moveForward(10);
+	printf("program finished, POMS should be in...\n");
+	return 0;
+}
+
+//convenience function to make code reading easier
+void moveBackward(int distanceInInches) {
+	moveForward(distanceInInches * -1);
+	printf("starting to move bwrds for %d\n",distanceInInches);
+	//convert inches to clicks
+	long clicks = -156.25l * distanceInInches;
+	mrp(RIGHT_MOTOR, SPEED_BWD, clicks);
+	mrp(LEFT_MOTOR, SPEED_BWD, clicks);
+	bmd(RIGHT_MOTOR);
+	bmd(LEFT_MOTOR);
+	
+	printf("done moving %d...", distanceInInches);
+
+}
+
+//uses mrp (move to relative position) and convert from inches
+//to motor units. This is pretty accurate (a lot more than motor at speed)
+void moveForward(int distanceInInches) {
+	printf("starting to move for %d\n",distanceInInches);
+	//convert inches to clicks
+	long clicks = 156.25l * distanceInInches;
+	mrp(RIGHT_MOTOR, SPEED_FWD, clicks);
+	mrp(LEFT_MOTOR, SPEED_FWD, clicks);
+	bmd(RIGHT_MOTOR);
+	bmd(LEFT_MOTOR);
+	
+	printf("done moving %d...", distanceInInches);
+}
+
+//right angle turn function
+void rightAngle(int direction) {
+	if (direction == RIGHT) {
+		printf("test turning right");
+		mrp(LEFT_MOTOR,SPEED_FWD,RIGHT_ANGLE_CLICKS);
+		bmd(LEFT_MOTOR);
+	} else if (direction == LEFT) {
+		printf ("test turning left");
+		mrp(RIGHT_MOTOR, SPEED_FWD, RIGHT_ANGLE_CLICKS) ;
+		bmd(RIGHT_MOTOR);
+	} else {
+		printf("ooopppsss I did not recognize your turn... so I ignored it");
+	}
+}
+	
