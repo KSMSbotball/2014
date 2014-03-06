1fd954a0b798be06ba484ecffee56f597ad8094f
KSMSbotball <ksmsbotball123@gmail.com>
2014-03-06 17:05:23 -0500

    it works but i needed to charge the kipr so didn't get to finish it today -_-

--- botball2014/B team programs/group B KIPR Alt/MainProgram.c
+++ botball2014/B team programs/group B KIPR Alt/MainProgram.c
@@ -38,7 +38,7 @@ void reset_counters();
 int main()
 {
 	printf("test 3.00, fixed fowd and bwd function bugs.\n");
-	//wait_for_light(2);
+	//wait_for_light(0);
 	shut_down_in(115);
 	enable_servos();
 	clawUp();
