5659c2f2869c8ad76cb9c738b56e53284701b403
KSMSbotball <ksmsbotball123@gmail.com>
2014-03-11 15:55:00 -0400

    started create program

--- botball2014/B team programs/group b Create/group b create.c
+++ botball2014/B team programs/group b Create/group b create.c
@@ -4,13 +4,16 @@ int main(){
 	printf("program started\n");
 	int create_connect();
 	printf("connected\n");
-	msleep(1500);
+	msleep(1500);ccc
 	Forward(10);
+	printf("Progarm Over\n");
 	create_disconnect();
 return 0;
 }
 void Forward(int inches){
-	create_drive_straight(400);
+	create_drive_direct(250,250); 
 	msleep(inches*0.0625);
-}
+} 
 
+
+	
