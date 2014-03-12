3ecead61d03f32e3786e94024bd74da651e6360e
KSMSbotball <botball2014>
2014-03-11 22:21:36 -0400

    revert changes from afternoon and enhance create pgm a tiny bit

--- botball2014/B team programs/group b Create/group b create.c
+++ botball2014/B team programs/group b Create/group b create.c
@@ -1,19 +1,30 @@
 // Created on Fri February 21 2014
-void Forward(int inches);
+#define SPEED_FWD 250
+#define SPEED_BWD -250
+void forward(int inches);
+void backwar(int inches);
+
+
 int main(){
 	printf("program started\n");
-	int create_connect();
+	create_connect();
 	printf("connected\n");
-	msleep(1500);ccc
-	Forward(10);
+	msleep(1500);
+	forward(10);
 	printf("Progarm Over\n");
 	create_disconnect();
-return 0;
+	return 0;
 }
-void Forward(int inches){
-	create_drive_direct(250,250); 
+void forward(int inches){
+	create_drive_straight(SPEED_FWD); 
 	msleep(inches*0.0625);
+	create_stop();
 } 
 
+void backward(int inches){
+	create_drive_straight(SPEED_BWD); 
+	msleep(inches*0.0625);
+	create_stop();
+} 
 
 	
