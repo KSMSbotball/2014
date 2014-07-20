111ca55ba11050f5f558c8659b12e45dec1921ce
KSMSbotball <ksmsbotball123@gmail.com>
2014-07-20 11:16:44 -0400

    Update

--- Charlie's Summer/Charliestest.c
+++ Charlie's Summer/Charliestest.c
@@ -1,23 +1,6 @@
 #include "KiprMovement.h"
 //Definitions Go at the TOP
 
-#define LEFT_PORT_WHEEL 2
-#define RIGHT_PORT_WHEEL 0
-#define FAST 1000
-#define SLOW 500
-
-
-
-//Method Declarations 
-void MoveForwardMillimeters(int mmToGo, int Speed);
-void GoStraight(int HowLong, int HowFast);
-void TurnLeft90();
-void TurnRight90();
-void GoBack(int Howlong);
-void Slant();
-void CorrectionRight(int HowFast,int HowLong);
-void TickCounter(int TicksToGo);
-//void BackUp();
 
 //Every C Program has a main() routine
 //This is where the program starts
@@ -43,42 +26,4 @@ int main ()
 	return 0;
 }
 
-//Methods defined at the top of the program
-//are put here with their complete code\
 
-void GoStraight(int HowLong, int HowFast)
-{
-	mav(RIGHT_PORT_WHEEL, HowFast);
-	mav(LEFT_PORT_WHEEL, HowFast);
-	msleep(HowLong);
-}
-void TurnRight90()
-{
-	mav(RIGHT_PORT_WHEEL, 0);
-	mav(LEFT_PORT_WHEEL,750 );
-	msleep(2300);
-}
-void TurnLeft90()
-{
-	mav(RIGHT_PORT_WHEEL, 750);
-	mav(LEFT_PORT_WHEEL, 0);
-	msleep(2200);	
-}
-void Slant()
-{
-	mav(RIGHT_PORT_WHEEL, 0);
-	mav(LEFT_PORT_WHEEL, 750);
-	msleep(1350);
-}
-void CorrectionRight(int HowFast,int HowLong)
-{
-	mav(RIGHT_PORT_WHEEL, 0);
-	mav(LEFT_PORT_WHEEL, HowFast);
-	msleep(HowLong);
-}
-//void BackUp()
-//{
-//mav(RIGHT_PORT_WHEEL, -750);
-//mav(LEFT_PORT_WHEEL, -750);
-//msleep(2000);
-//}
--- Charlie's Summer/KiprTests.h
+++ Charlie's Summer/KiprTests.h
@@ -1,11 +1,9 @@
-// Created on Sun July 20 2014
 
 #ifndef _KIPRTESTS_H_
 #define _KIPRTESTS_H_
 
-#define LEFT_MOTOR 2
-#define RIGHT_MOTOR 0
 
 void TickCounter(int TicksToGo);
+
 
 #endif
